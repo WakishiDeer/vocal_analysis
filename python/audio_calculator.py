b8908828e1ea1f288a00b25ffa3f95deb1d7b8c3
WakishiDeer <by9132@gmail.com>
2022-05-02 18:30:34 +0900

    add f0 calculation

--- python/audio_calculator.py
+++ python/audio_calculator.py
@@ -9,6 +9,9 @@ class AudioCalculator:
     This class calculates audio data.
     The variety of speach analysis are following:
     {pause, volume, pitch, speed}
+    Todo:
+        add feature -> neural model based vad with speech segmentation, splitting it into sentences and words.
+        To do above, we can calculate features based on each sentence and word.
     Notes:
         This class has audio dependencies such as `auditok` and `librosa`.
     """
@@ -17,10 +20,11 @@ class AudioCalculator:
         self.logger = Logger(name=__name__)
 
     def vad_generator(self, audio_data: np.ndarray, min_dur_sec: float = 0.2, max_dur_sec: float = 5,
-                      max_silence_sec: float = 0.5, energy_threshold: float = 50.0):
+                      max_silence_sec: float = 0.5, energy_threshold: float = 50.0, sample_rate=16000):
         """
         Vad based on energy.
         Args:
+            sample_rate:
             energy_threshold:
             max_dur_sec:
             min_dur_sec:
@@ -34,7 +38,7 @@ class AudioCalculator:
         Todo:
             Conduct an experiment to find what values of args are good for my research.
         """
-        audio_region = auditok.AudioRegion(data=audio_data.tobytes(), sampling_rate=16000, sample_width=2,
+        audio_region = auditok.AudioRegion(data=audio_data.tobytes(), sampling_rate=sample_rate, sample_width=2,
                                            channels=1)
         # res = audio_region.split(
         res = auditok.split(
@@ -75,7 +79,7 @@ class AudioCalculator:
     def calc_energy_rms(self, magnitude: np.ndarray = None, voiced_audio_data: np.ndarray = None,
                         frame_length: int = 512,
                         hop_length: int = 512 // 4,
-                        is_freq: bool = False):
+                        is_freq: bool = False) -> np.ndarray:
         """
         Calc root-mean-square of energy of each region.
         Args:
@@ -93,7 +97,7 @@ class AudioCalculator:
             res = librosa.feature.rms(y=voiced_audio_data, frame_length=frame_length, hop_length=hop_length)
         return res
 
-    def calc_average_energy_rms(self, root_energy: np.ndarray = None):
+    def calc_average_energy_rms(self, root_energy: np.ndarray = None) -> np.float32:
         """
         Calc average for root-mean-square of energy.
         Args:
@@ -102,6 +106,42 @@ class AudioCalculator:
         """
         res = np.mean(root_energy)
         return res
+
+    def calc_f0(self, voiced_audio_data: np.ndarray = None, sample_rate: int = 16000,
+                frame_length: int = 512,
+                hop_length: int = 512 // 4,
+                min_freq: int = librosa.note_to_hz("C2"), max_freq: int = librosa.note_to_hz("C7")) -> [np.ndarray,
+                                                                                                        np.ndarray,
+                                                                                                        np.ndarray,
+                                                                                                        np.ndarray]:
+        """
+        References:
+            https://librosa.org/doc/main/generated/librosa.pyin.html
+            Mauch, Matthias, and Simon Dixon.
+            “pYIN: A fundamental frequency estimator using probabilistic threshold distributions.”
+             2014 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP). IEEE, 2014.
+        Args:
+            hop_length:
+            frame_length:
+            max_freq:
+            min_freq:
+            voiced_audio_data:
+            sample_rate:
+        Returns:
+            f0: time series of fundamental frequencies in Hertz.
+            voiced_flag: time series containing boolean flags indicating whether a frame is voiced or not.
+            voiced_probs: time series containing the probability that a frame is voiced.
+
+            times:
+        """
+        f0, voiced_flag, voiced_probs = librosa.pyin(y=voiced_audio_data, fmin=min_freq, fmax=max_freq, sr=sample_rate,
+                                                     frame_length=frame_length, hop_length=hop_length)
+        times = librosa.times_like(f0, hop_length=hop_length, sr=sample_rate)
+        return f0, voiced_flag, voiced_probs, times
+
+    def calc_average_f0(self, f0: np.ndarray = np.array([])) -> np.float32:
+        f0_avg = np.mean(f0)
+        return f0_avg
 
     def calc_normalization(self, audio_data: np.ndarray):
         """
--- python/audio_stream.py
+++ python/audio_stream.py
@@ -1,4 +1,5 @@
 import numpy as np
+import auditok
 
 import queue
 
@@ -15,7 +16,7 @@ class AudioStream(Audio):
     And an instance of this class will be used in `AudioController` class in `audio_handler.py`
     """
 
-    def __init__(self, audio_manipulator=None, audio_calculator=None, zeromq_sender=None):
+    def __init__(self, audio_manipulator=None, audio_calculator=None, zeromq_sender=None) -> None:
         """
         Initialize sound device to decide which one to select for audio_util stream.
         """
@@ -31,22 +32,27 @@ class AudioStream(Audio):
             self.logger.exception("Setting for input device hasn't done yet.")
         else:
             # setting for input device
-            self.DOWN_SAMPLE = 20
-            self.CHUNK_DURATION_MS = 25 * 40 * 5
-            self.FRAME_LENGTH = int(  # number of overall frames per callback
+            self.DOWN_SAMPLE: int = 20
+            self.CHUNK_DURATION_MS: int = 25 * 40 * 5
+            self.FRAME_LENGTH: int = int(  # number of overall frames per callback
                 self.audio_manipulator.INPUT_SAMPLE_RATE * self.CHUNK_DURATION_MS / 1000)
-            self.WINDOW_LENGTH = 512  # length for each sliding process
-            self.HOP_LENGTH = self.WINDOW_LENGTH // 4  # usually, one-fourth of WINDOW_LENGTH
-            self.buffer = queue.Queue()  # this is for plot
-            self.region_concat = None  # `auditok.AudioRegion` can use operator `+` to concatenate
-            self.region_concat_energy = None
+            self.WINDOW_LENGTH: int = 512  # length for each sliding process
+            self.HOP_LENGTH: int = self.WINDOW_LENGTH // 4  # usually, one-fourth of WINDOW_LENGTH
+            self.buffer: queue.Queue = queue.Queue()  # this is for plot
+
+            # audio data
+            self.region_concat: auditok.AudioRegion = auditok.AudioRegion(
+                data=np.array([]).tobytes())  # `auditok.AudioRegion` can use operator `+` to concatenate
+            self.region_concat_energy: np.ndarray = np.array([])
+            self.region_concat_f0: np.ndarray = np.array([])
+            self.region_concat_f0_flags: np.ndarray = np.array([])
             self.stream = None
 
             # when exiting, save the voiced region for sample
             import atexit
             atexit.register(self.save_region)
 
-    def audio_callback_numpy(self, indata: np.ndarray, frames: int, time, status):
+    def audio_callback_numpy(self, indata: np.ndarray, frames: int, time, status) -> None:
         """
         This callback will be called from each audio_util block.
         After calculation, the dict of values will send to Unity Process.
@@ -74,19 +80,24 @@ class AudioStream(Audio):
     def audio_callback_raw(self, indata, frames: int, time, status):
         pass
 
-    def handle_calculation(self, indata: np.ndarray = None):
+    def handle_calculation(self, indata: np.ndarray = None) -> None:
         """
         This class specifies calculation for each callback (i.e., for each block)
         Args:
             indata (np.ndarray): This is audio_util data whose shape will be (`self.block_size`, `sd.default.channels[0]`).
         Returns:
         """
-        # for each voiced region
+        # extracts voiced region
         vad_generator = self.audio_calculator.vad_generator(audio_data=indata,
                                                             max_dur_sec=self.CHUNK_DURATION_MS / 1000)
-        region_info = str()
-        root_energy = None
+
+        region_info: str = str()
+        root_energy: np.ndarray = np.array([])
         for i, region in enumerate(vad_generator):
+            # to save the region
+            if Profile.is_init:
+                Profile.is_writable = True
+                Profile.is_init = False
             # get values represented as np.ndarray
             voiced_audio_data = self.audio_manipulator.int_to_float32(audio_data=region.samples)
             # calc stft
@@ -99,23 +110,24 @@ class AudioStream(Audio):
             root_energy = self.audio_calculator.calc_energy_rms(magnitude=magnitude,
                                                                 frame_length=512,
                                                                 is_freq=is_freq)
+            # calculate f0
+            f0, voiced_flag, _, times = self.audio_calculator.calc_f0(voiced_audio_data=voiced_audio_data)
+
+            # concat region info
             region_info += "#{} region: {}sec detected.\n".format(i, region.duration)
-            # switch on the first time or not
-            if Profile.is_init:
-                self.region_concat = region
-                self.region_concat_energy = np.array([root_energy])
-                Profile.is_writable = True
-                Profile.is_init = False
-            else:
-                # store voiced regions and energy
-                self.region_concat = region + self.region_concat
-                self.region_concat_energy = np.append(self.region_concat_energy, root_energy)
+            # concat voiced regions
+            self.region_concat = region + self.region_concat
+            # concat energy
+            self.region_concat_energy = np.append(self.region_concat_energy, root_energy)
+            # concat f0 and its flag (voiced or not)
+            self.region_concat_f0 = np.append(self.region_concat_f0, f0)
+            self.region_concat_f0_flags = np.append(self.region_concat_f0_flags, voiced_flag)
+
+        # if the voiced region was not found
         if region_info != "":
             self.logger.logger.info(region_info)
             self.average_energy_rms = self.audio_calculator.calc_average_energy_rms(root_energy=root_energy)
-            print("avg rms of energy: ",
-                  self.average_energy_rms,
-                  "\n")
+            # print("avg rms of energy: ", self.average_energy_rms, "\n")
 
     def store_values(self):
         """
@@ -124,7 +136,7 @@ class AudioStream(Audio):
         """
         pass
 
-    def get_input_stream_numpy(self):
+    def get_input_stream_numpy(self) -> sd.InputStream:
         """
         Default samplerate and farame_width of input device are written here.
         Returns:
@@ -137,7 +149,7 @@ class AudioStream(Audio):
             blocksize=self.FRAME_LENGTH
         )
 
-    def get_input_stream_raw(self):
+    def get_input_stream_raw(self) -> sd.RawInputStream:
         return sd.RawInputStream(
             dtype="int16",
             device=sd.default.device[0],
@@ -145,7 +157,7 @@ class AudioStream(Audio):
             blocksize=self.FRAME_LENGTH
         )
 
-    def save_region(self):
+    def save_region(self) -> None:
         """
         When exiting, save the voiced audio regions, which is concatenated.
         Returns:
